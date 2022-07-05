03f4da064ef44ea8a6ec3b8f8ec9d207cd551e5b
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2022-07-05 01:08:27 -0400

    added dotenv

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {MoralisProvider} from 'react-moralis'
+// require('dotenv').config()
+import 'dotenv/config'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
