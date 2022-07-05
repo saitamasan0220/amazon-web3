d322caf660b8549aaa69f8a69acd579d3df3fcfa
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2022-07-05 01:15:06 -0400

    issues with env variables

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,19 @@ import type { AppProps } from 'next/app'
 import {MoralisProvider} from 'react-moralis'
 // require('dotenv').config()
 import 'dotenv/config'
+import App from 'next/app'
+
+const AppId = process.env.NEXT_PUBLIC_MORALIS_SERVER
+const ServerUrl=process.env.NEXT_PUBLIC_MORALIS_APP_ID
+console.log("App ID: ", AppId, " ServerUrl: ", ServerUrl)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MoralisProvider
-      serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER}  
-      appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
+      // serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER}  
+      // appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
+      serverUrl={ServerUrl}  
+      appId={AppId}
     >
       <Component {...pageProps} />
     </MoralisProvider>
