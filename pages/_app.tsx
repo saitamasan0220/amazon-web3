6a65fed08647d77596a9d88bf204e599fe714b30
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2022-07-05 23:12:31 -0400

    Sidebar component done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {MoralisProvider} from 'react-moralis'
-// require('dotenv').config()
-// import 'dotenv/config'
 import App from 'next/app'
 
 const AppId = process.env.NEXT_PUBLIC_MORALIS_SERVER
@@ -13,19 +11,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
       <MoralisProvider
-        // serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER}  
-        // appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
-        // serverUrl={ServerUrl}  
-        // appId={AppId}
-        // serverUrl='https://vb1xoxbywv7i.usemoralis.com:2053/server'  
-        // appId='wiwXEfK7g8IAWMFFgXaDqThTwOgKKcgjGSxu9fQQ'
         serverUrl={`${process.env.NEXT_PUBLIC_MORALIS_SERVER}`}  
         appId={`${process.env.NEXT_PUBLIC_MORALIS_APP_ID}`}
       >
         <Component {...pageProps} />
       </MoralisProvider>
     
-      <div>{process.env.NEXT_PUBLIC_MORALIS_SERVER}</div>
+      {/* <div>{process.env.NEXT_PUBLIC_MORALIS_SERVER}</div>s */}
     </div>
   )
 }
